01198f02325102481e5ef723d5a09b7f5c1c9db6
xinyang1328 <xinyang1328@gmail.com>
2020-05-07 15:41:53 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,16 @@
+import {
+    hostname,
+    port
+} from './public/environment'
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ public/environment.js
@@ -0,0 +1,3 @@
+export default hostname = '127.0.0.1'
+
+export default port = '9000'
